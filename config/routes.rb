d04387f18b37588efab9991772dc9ea32aa38b7e
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-04-12 19:49:42 +0900

    feat: ユーザー一覧ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  resources :users, only: %i[index]
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
