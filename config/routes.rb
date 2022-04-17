73b5664b712a8f54a387d49a81b51ab73bc50a67
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-04-17 13:34:03 +0900

    feat: プライバシーポリシーと利用規約のページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'home#index'
   resources :users, only: %i[index]
+  get '/privacy', to: 'home#privacy'
+  get '/terms', to: 'home#terms'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
