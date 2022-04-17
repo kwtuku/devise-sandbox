608ba4ea6f1825dcdc7261c49672c70512ef7cb4
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-04-18 00:20:28 +0900

    feat: Twitter認証機能を追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -19,6 +19,11 @@ module Users
       sign_in_and_redirect @user
     end
 
+    def twitter
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      sign_in_and_redirect @user
+    end
+
     # More info at:
     # https://github.com/heartcombo/devise#omniauth
 
